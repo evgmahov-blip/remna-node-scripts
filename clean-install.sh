0100f67c3dbd5d0ea57e53c2b0718af85b7b9f56
EugeneM <evgmahov@gmail.com>
2026-09-19 15:54:19 +0300

    Advance clean-install.sh to menu-return launcher

--- clean-install.sh
+++ clean-install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-PINNED_REF="787f092d4d9e287d27872a79040b3b8c70f75c7c"
-EXPECTED_BLOB_SHA="ac7842a2baa37d03229b13823c0930d0cb2b92b3"
+PINNED_REF="2ec61bfee7e17d4746f264b695ca6f07d710fa02"
+EXPECTED_BLOB_SHA="4293d5aa8b81426b91ec4696026cf1991b0e8bc1"
 URL="https://raw.githubusercontent.com/evgmahov-blip/remna-node-scripts/${PINNED_REF}/full-clean-reinstall.sh"
 TMP="$(mktemp)"
 trap 'rm -f "$TMP"' EXIT
