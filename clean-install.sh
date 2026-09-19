a83bdb8526ea17673bb1b412997a4857a039de02
EugeneM <evgmahov@gmail.com>
2026-09-19 16:03:46 +0300

    Advance clean-install.sh to Config Profile launcher

--- clean-install.sh
+++ clean-install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-PINNED_REF="fec10af71f89a6d3ea91950acf9bff08b3682e52"
-EXPECTED_BLOB_SHA="f2de5358a078209fc37aa67a08b1804b8671673d"
+PINNED_REF="bbb3c05613657653269c7e8e2f5c24bd6949c3b2"
+EXPECTED_BLOB_SHA="628f194c90822b551277da3384561681cf3a81d2"
 URL="https://raw.githubusercontent.com/evgmahov-blip/remna-node-scripts/${PINNED_REF}/full-clean-reinstall.sh"
 TMP="$(mktemp)"
 trap 'rm -f "$TMP"' EXIT
