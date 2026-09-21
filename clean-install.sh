259fde6f9afdbb5fe32d6124b93888137eb8c6a5
EugeneM <evgmahov@gmail.com>
2026-09-21 21:25:21 +0300

    Repin clean-install.sh after focused suite guard fix

--- clean-install.sh
+++ clean-install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-PINNED_REF="a54d3c4bba01e81c0b34fc949e4b4d1e3b556885"
-EXPECTED_BLOB_SHA="57a722aabe5e6674367df9c62cd62f317fc48738"
+PINNED_REF="90bec37774d281a901958b40d8efc2cfe21668b6"
+EXPECTED_BLOB_SHA="55b1b821cad2acd320ee55a086677178d27d2668"
 URL="https://raw.githubusercontent.com/evgmahov-blip/remna-node-scripts/${PINNED_REF}/full-clean-reinstall.sh"
 TMP="$(mktemp)"
 trap 'rm -f "$TMP"' EXIT
