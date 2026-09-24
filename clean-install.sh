41bfba0f30fa7bacba70583a81e8a084df067fa7
EugeneM <evgmahov@gmail.com>
2026-09-24 12:18:43 +0300

    fix: pin launcher to expanded node analysis

--- clean-install.sh
+++ clean-install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-PINNED_REF="f04a2767325c1f47b9bc693a51e0429b46c1b6f1"
-EXPECTED_BLOB_SHA="4af3252427f7da6dcc33a08b1c18fcf2ad327795"
+PINNED_REF="e300638b4d748f018bae0eb9f466a915c239649b"
+EXPECTED_BLOB_SHA="abb0664ad95adf163f8f0fa7370ec4f06c8fa3fe"
 URL="https://raw.githubusercontent.com/evgmahov-blip/remna-node-scripts/${PINNED_REF}/full-clean-reinstall.sh"
 TMP="$(mktemp)"
 trap 'rm -f "$TMP"' EXIT
