a2f5c904cf9033cb2480521b439fce2670c50c3a
EugeneM <evgmahov@gmail.com>
2026-09-24 15:43:13 +0300

    chore: repin corrected server multitest

--- full-clean-reinstall.sh
+++ full-clean-reinstall.sh
@@ -19,8 +19,8 @@ V2_CLEANUP_URL="https://raw.githubusercontent.com/${REPO}/${V2_CLEANUP_REF}/next
 NETWORK_REF="8378a6b4340fc0b11b3f66246caaa39d3ee360b9"
 NETWORK_BLOB_SHA="a5157e7c48f3e2a1c4df4676ecd4a51511d15949"
 NETWORK_URL="https://raw.githubusercontent.com/${REPO}/${NETWORK_REF}/next-installer/network-tuning-manager.sh"
-TESTER_REF="7ad87ee310ae809fbd8e5b90c7468890333f1414"
-TESTER_BLOB_SHA="b304410029c163b45fee34aa1b61ac0c4bf58ae1"
+TESTER_REF="68dfdd2fc69cf5d2d0e97b4613b1f375a36e0a73"
+TESTER_BLOB_SHA="47e1e8e8ec1498d84d6e2f2627facbea9711bae5"
 TESTER_URL="https://raw.githubusercontent.com/${REPO}/${TESTER_REF}/next-installer/server-multitest.sh"
 
 MGMT_OVERLAY_REF="f7c609b0fa929cecc4a74db5bafe90b0a4d782be"
